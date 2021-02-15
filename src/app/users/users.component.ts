4043d87d363d9308e5660e467372af9efa05d01b
Elizabeth Sharp <esharp@nrao.edu>
2021-02-14 21:57:18 -0500

    refactored to output ACSII table to a <pre> tag

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,7 @@
 import { IUser } from '../igithub';
 import { OrganizationsService } from './users.service';
 import { Component, OnInit } from '@angular/core';
+import { SSL_OP_NETSCAPE_CA_DN_BUG } from 'constants';
 
 @Component({
   selector: 'app-users',
@@ -12,9 +13,30 @@ export class UsersComponent implements OnInit {
   constructor(public service: OrganizationsService) { }
 
   users: IUser[] = [];
+  userTable: string = "";
 
   ngOnInit(): void {
-    this.fetchUsers(10);
+    this.fetchUsersAcsii(100);
+  }
+
+  fetchUsersAcsii(count: number) {
+    this.service.fetchUsers(count)
+      .then(res => {
+        console.log("fetched result: " + JSON.stringify(res));
+        let objs: IUser[] = res;
+        objs.forEach(user => this.userTable = this.userTable.concat( 
+          user.id.toString().padEnd(5, " ") + 
+          user.login.padEnd(20, " ") + 
+          user.type.padEnd(14, " ") + 
+          new Boolean(user.site_admin).toString().padEnd(5, " ")
+           + `\r\n`));
+
+       })
+      .catch(err => {
+        console.error(err);
+        alert("ERROR!: " + JSON.stringify( err.status ));
+      });
+
   }
 
   fetchUsers(count: number) {
@@ -22,11 +44,14 @@ export class UsersComponent implements OnInit {
       .then(res => {
         console.log("fetched result: " + JSON.stringify(res));
         let obj: IUser[] = res;
-        this.users = obj;
+        for (let i = 0; i < obj.length; i++) {
+          console.log();
+        }
+        this.users = obj;//JSON.stringify(res);
       })
       .catch(err => {
         console.error(err);
-        alert("ERROR!: " + JSON.stringify(err.status));
+        alert("ERROR!: " + JSON.stringify( err.status ));
       });
 
   }
